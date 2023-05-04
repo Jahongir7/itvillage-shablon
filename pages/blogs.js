31ff4b7ddaaf3f2dcd8f68465f6c669e7a9f5414
jahongir7 <jahongir95119@gmail.com>
2023-05-04 14:11:37 +0500

    first commit

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,15 +1,11 @@
-import { Blog } from "@/sections"
-import Head from "next/head"
+import { Blog } from "@/sections";
 
 const Blogs = () => {
   return (
     <>
-      <Head>
-        <title>Blog - 7Creative</title>
-      </Head>
       <Blog />
     </>
-  )
-}
+  );
+};
 
-export default Blogs
+export default Blogs;
